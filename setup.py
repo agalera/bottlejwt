37f18af73632953292a813b9b50361f1d357942c
Alberto Galera <galerajimenez@gmail.com>
2016-09-26 21:06:31 +0200

    prepare package

---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from pypandoc import convert
+    read_md = lambda f: convert(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found")
+    read_md = lambda f: open(f, 'r').read()
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python3 setup.py sdist upload')
+    sys.exit()
+
+readme = read_md('README.md')
+
+
+setup(
+    name='bottle-jwt',
+    version='0.0.1',
+    description='python events non-blocking',
+    long_description=readme,
+    author='Alberto Galera Jimenez',
+    author_email='galerajimenez@gmail.com',
+    url='https://github.com/kianxineki/bottle_jwt',
+    py_modules=['bottle_jwt'],
+    include_package_data=True,
+    install_requires=['pyjwt', 'bottle'],
+    license="GPL",
+    zip_safe=False,
+    keywords='bottle_jwt',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3'
+    ],
+    entry_points={}
+)
