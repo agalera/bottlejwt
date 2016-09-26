ad4e7144f165cf54de8d17c5e0de896f2536eb49
Alberto Galera <galerajimenez@gmail.com>
2016-09-26 21:07:40 +0200

    add example

---
+++ examples/server.py
@@ -0,0 +1,15 @@
+from bottle import get, install, run
+from bottle_jwt import JwtPlugin
+
+def validation(auth, auth_value):
+    print(auth, auth_value)
+    return True
+
+@get("/", auth="any values and types")
+def example(auth):  # auth argument is optional!
+    return "ok"
+
+
+install(JwtPlugin(validation, 'secret'))
+run(host="0.0.0.0", port="9988")
+
